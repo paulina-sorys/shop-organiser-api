e1822566aad5fe33b063a112405c847334b39d2f
PaulinaSorys <paullina.sorys@gmail.com>
2023-06-03 17:40:58 +0200

    Create dummy products-all API endpoint

---
+++ server/server_test.go
@@ -0,0 +1,21 @@
+package server
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGETProductsAll(t *testing.T) {
+	request, _ := http.NewRequest(http.MethodGet, "api/v1/products/all", nil)
+	response := httptest.NewRecorder()
+
+	Server(response, request)
+
+	got := response.Body.String()
+	want := "20"
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
